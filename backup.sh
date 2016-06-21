3e0fc0c324296ceee40a2d87182137f48ded47d0
Andrew Cutler <macropin@gmail.com>
2016-06-21 10:46:01 +1000

    standardize

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ echo "==> Running Backup"
 # Defaults 
 
 : ${BACKUP_KEEP:=30}  # Keep 30 days
-: ${BACKUP_DEST:='/mnt/s3'}
+: ${BACKUP_DST:='/mnt/s3'}
 : ${BACKUP_SRC:=''}
 : ${BACKUP_TMP:='/tmp'}
 : ${BACKUP_NAME:=$(date +%Y%m%d%H%M%S)}
@@ -18,9 +18,9 @@ echo "==> Running Backup"
 
 # Perform backup
 tar -czf ${BACKUP_TMP}/${BACKUP_NAME}.tgz $BACKUP_SRC
-mv ${BACKUP_TMP}/${BACKUP_NAME}.tgz $BACKUP_DEST
+mv ${BACKUP_TMP}/${BACKUP_NAME}.tgz $BACKUP_DST
 
 # cleanup
-find $BACKUP_DEST -type f -mtime +${BACKUP_KEEP} -exec rm -f {} \;
+find $BACKUP_DST -type f -mtime +${BACKUP_KEEP} -exec rm -f {} \;
 
 echo "Done"
