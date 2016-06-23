497981116bd765fdeaff3ce333980d69980b19ca
Andrew Cutler <macropin@gmail.com>
2016-06-23 11:53:13 +1000

    add support for encrypted backups

--- backup.sh
+++ backup.sh
@@ -13,6 +13,7 @@ echo "==> Running Backup"
 : ${BACKUP_TMP:='/tmp'}
 : ${BACKUP_NAME:=$(date +%Y%m%d%H%M%S)}
 : ${BACKUP_EXCLUDE:=''}
+: ${BACKUP_PASSPHRASE:=''}
 
 # Tests
 [ "$BACKUP_SRC" == "" ] && echo "Error: BACKUP_SRC not specified" && exit 128
@@ -21,8 +22,13 @@ echo "==> Running Backup"
 [ "$BACKUP_EXCLUDE" != "" ] && for I in $BACKUP_EXCLUDE; do EXCLUDE_ARGS+="--exclude ${I} "; done
 
 # Perform backup
-tar -czf ${BACKUP_TMP}/${BACKUP_NAME}.tgz $EXCLUDE_ARGS $BACKUP_SRC
-mv ${BACKUP_TMP}/${BACKUP_NAME}.tgz $BACKUP_DST
+if [ "$BACKUP_PASSPHRASE" == "" ]; then
+    tar -czf ${BACKUP_TMP}/${BACKUP_NAME}.tgz $EXCLUDE_ARGS $BACKUP_SRC
+    mv ${BACKUP_TMP}/${BACKUP_NAME}.tgz $BACKUP_DST
+else
+    tar -cz $EXCLUDE_ARGS $BACKUP_SRC | openssl enc -e -aes-256-cbc -salt -k $BACKUP_PASSPHRASE -out ${BACKUP_TMP}/${BACKUP_NAME}.tgz.enc
+    mv ${BACKUP_TMP}/${BACKUP_NAME}.tgz.enc $BACKUP_DST
+fi
 
 # cleanup
 find $BACKUP_DST -type f -mtime +${BACKUP_KEEP} -exec rm -f {} \;
